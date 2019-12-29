db5e10d2e49d97d610b40405344c14c435d5664d
shigaki daichi <d-shigaki@pro-seeds.co.jp>
2019-12-29 15:40:45 +0900

    wip

--- training/lib/main.dart
+++ training/lib/main.dart
@@ -20,7 +20,15 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
+        accentColor: Colors.black,
+        buttonTheme: ButtonThemeData(
+          textTheme: ButtonTextTheme.accent
+        ),
+        brightness: Brightness.light
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
       ),
       // home: FirstPage(),
       initialRoute: '/',
@@ -29,93 +37,9 @@ class MyApp extends StatelessWidget {
         '/second': (context) => SecondPage(),
         '/third': (context) => ThirdPage(),
         '/forth': (context) => ForthPage(),
-        '/form': (context) => FormSamplePage()
+        '/form': (context) => FormSamplePage(),
+        '/material': (context) => MaterialPage()
       },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
@@ -123,7 +47,9 @@ class _MyHomePageState extends State<MyHomePage> {
 class FirstPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('First Page')),
+      appBar: AppBar(
+        title: const Text('First Page'),
+      ),
       body: Center(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -147,18 +73,24 @@ class FirstPage extends StatelessWidget {
               },
               child: Text('Third Page'),
             ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/forth');
-              },
-              child: Text('Forth Page'),
-            ),
+            // RaisedButton(
+            //   onPressed: () {
+            //     Navigator.pushNamed(context, '/forth');
+            //   },
+            //   child: Text('Forth Page'),
+            // ),
             RaisedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/form');
               },
               child: Text('Form Page'),
             ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/material');
+              },
+              child: Text('Material Page'),
+            )
           ],
         ),
       ),
@@ -241,6 +173,46 @@ class ThirdPage extends StatelessWidget {
   }
 }
 
+class MaterialPage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    var messageFromFirstPage = ModalRoute.of(context).settings.arguments;
+    print(messageFromFirstPage);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Material Page'),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            itemBuilder: (BuildContext context) => <PopupMenuItem<String>>[
+              const PopupMenuItem<String>(
+                value: 'Toolbar Menu',
+                child: Text('Toolbar Menu'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'Hoge Menu',
+                child: Text('Hoge Menu'),
+              ),
+            ],
+          )
+        ],
+        ),
+        body: Center(
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0)
+            ),
+            child: Column(
+              children: <Widget>[
+                const ListTile(
+                  leading: Icon(Icons.album),
+                  title: Text('title'),
+                ),
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
 class ForthPage extends StatelessWidget {
   Widget build(BuildContext context) {
     var messageFromFirstPage = ModalRoute.of(context).settings.arguments;
@@ -265,46 +237,84 @@ class ForthPage extends StatelessWidget {
 }
 
 class FormSamplePage extends StatelessWidget {
+  int _selectedIndex = 0;
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Form')),
-        body: Form(
-          key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  decoration: const InputDecoration(
-                    filled: true,
-                    icon: Icon(Icons.person),
-                    hintText: '名前を入力して下さい',
-                    labelText: '名前'
-                  ),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return '必須です';
-                    }
-                    return null;
-                  },
-                ),
-                RaisedButton(
-                  color: Colors.blue,
-                  textColor: Colors.white,
-                  onPressed: () {
-                    if (_formKey.currentState.validate()) {
-                      Scaffold.of(context).showSnackBar(const SnackBar(
-                        content: Text('Processing Data'),
-                      ));
-                    }
-                  },
-                  child: const Text('Sumbit'),
-                ),
+      // home: Scaffold(
+      //   appBar: AppBar(title: const Text('Form')),
+      //   body: Form(
+      //     key: _formKey,
+      //     child: Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: Column(
+      //         children: <Widget>[
+      //           TextFormField(
+      //             decoration: const InputDecoration(
+      //                 filled: true,
+      //                 icon: Icon(Icons.person),
+      //                 hintText: '名前を入力して下さい',
+      //                 labelText: '名前'),
+      //             validator: (value) {
+      //               if (value.isEmpty) {
+      //                 return '必須です';
+      //               }
+      //               return null;
+      //             },
+      //           ),
+      //           RaisedButton(
+      //             color: Colors.blue,
+      //             textColor: Colors.white,
+      //             onPressed: () {
+      //               if (_formKey.currentState.validate()) {
+      //                 Scaffold.of(context).showSnackBar(const SnackBar(
+      //                   content: Text('Processing Data'),
+      //                 ));
+      //               }
+      //             },
+      //             child: const Text('Sumbit'),
+      //           ),
+      //         ],
+      //       ),
+      //     ),
+      //   ),
+      // ),
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike))
               ],
             ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text('Home'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.business),
+                title: Text('Business'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.school),
+                title: Text('School'),
+              )
+            ],
+            currentIndex: _selectedIndex,
           ),
         ),
       ),
