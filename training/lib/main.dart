69510ad65f96574fe1df969d763f735571545b7d
leica19 <spike-19@ezweb.ne.jp>
2019-12-31 13:16:15 +0900

    wip

--- training/lib/main.dart
+++ training/lib/main.dart
@@ -1,6 +1,7 @@
-import 'dart:io';
+import 'dart:convert' show jsonDecode;
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -11,25 +12,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      // home: FirstPage(),
+      darkTheme: ThemeData(brightness: Brightness.dark),
       initialRoute: '/',
       routes: {
         '/': (context) => FirstPage(),
         '/second': (context) => SecondPage(),
         '/third': (context) => ThirdPage(),
-        '/forth': (context) => ForthPage(),
-        '/form': (context) => FormSamplePage()
+        // '/forth': (context) => ForthPage(),
+        '/form': (context) => MainFetchData()
       },
     );
   }
@@ -37,15 +29,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -58,11 +41,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -85,20 +63,6 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -128,18 +92,6 @@ class FirstPage extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            // RaisedButton(
-            //   onPressed: () {
-            //     Navigator.pushNamed(context, '/second',
-            //         arguments: 'messageFromFirstPage');
-            //   },
-            //   child: Text('Second Page'),
-            // ),
-            // Expanded(child: RaisedButton(
-            //   onPressed: () {
-            //   },
-            //   child: Text('to Where?'),
-            // )),
             RaisedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/third',
@@ -190,19 +142,6 @@ class ThirdPage extends StatelessWidget {
     print(messageFromFirstPage);
     return Scaffold(
       appBar: AppBar(title: const Text('Third Page')),
-      // body: Container(
-      //   alignment: Alignment.center,
-      //   color: Colors.green,
-      //   width: 300,
-      //   height: 300,
-      //   child: RaisedButton(
-      //     onPressed: () {
-      //       Navigator.pop(context, 'messageFromSecondPage');
-      //     },
-      //     child: Text('Go Back'),
-      //   ),
-      // ),
-
       body: GridView(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             mainAxisSpacing: 8,
@@ -221,22 +160,6 @@ class ThirdPage extends StatelessWidget {
           myContainer('6')
         ],
       ),
-
-      // body: SingleChildScrollView(
-      //   child: Column(
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: <Widget>[
-      //       myContainer('dada'),
-      //       myContainer('dada'),
-      //       myContainer('dada'),
-      //       myContainer('dada'),
-      //       myContainer('dada'),
-      //       myContainer('dada'),
-      //       myContainer('dada'),
-      //       myContainer('dada'),
-      //     ],
-      //   ),
-      // ),
     );
   }
 }
@@ -264,54 +187,6 @@ class ForthPage extends StatelessWidget {
   }
 }
 
-class FormSamplePage extends StatelessWidget {
-  final _formKey = GlobalKey<FormState>();
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Form')),
-        body: Form(
-          key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  decoration: const InputDecoration(
-                    filled: true,
-                    icon: Icon(Icons.person),
-                    hintText: '名前を入力して下さい',
-                    labelText: '名前'
-                  ),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return '必須です';
-                    }
-                    return null;
-                  },
-                ),
-                RaisedButton(
-                  color: Colors.blue,
-                  textColor: Colors.white,
-                  onPressed: () {
-                    if (_formKey.currentState.validate()) {
-                      Scaffold.of(context).showSnackBar(const SnackBar(
-                        content: Text('Processing Data'),
-                      ));
-                    }
-                  },
-                  child: const Text('Sumbit'),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
 Container myContainer(String text) {
   return Container(
     alignment: Alignment.center,
@@ -320,4 +195,77 @@ Container myContainer(String text) {
     color: Colors.green,
     child: Text(text),
   );
+}
+
+class MainFetchData extends StatefulWidget {
+  @override
+  _MainFetchDataState createState() => _MainFetchDataState();
+}
+
+class _MainFetchDataState extends State<MainFetchData> {
+  List<Photo> list = List();
+  var isLoading = false;
+
+  _fetchData() async {
+    setState(() {
+      isLoading = true;
+    });
+    final response =
+        await http.get("https://jsonplaceholder.typicode.com/photos");
+    if (response.statusCode == 200) {
+      list = (jsonDecode(response.body) as List)
+          .map((data) => new Photo.fromJson(data))
+          .toList();
+      setState(() {
+        isLoading = false;
+      });
+    } else {
+      throw Exception('Failed to load photos');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Fetch Data JSON"),
+        ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: RaisedButton(
+            child: new Text("Fetch Data"),
+            onPressed: _fetchData,
+          ),
+        ),
+        body: isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView.builder(
+                itemCount: list.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    contentPadding: EdgeInsets.all(10.0),
+                    title: new Text(list[index].title),
+                    trailing: new Image.network(
+                      list[index].thumbnailUrl,
+                      fit: BoxFit.cover,
+                      height: 40.0,
+                      width: 40.0,
+                    ),
+                  );
+                }));
+  }
+}
+
+class Photo {
+  final String title;
+  final String thumbnailUrl;
+  Photo._({this.title, this.thumbnailUrl});
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return new Photo._(
+      title: json['title'],
+      thumbnailUrl: json['thumbnailUrl'],
+    );
+  }
 }
